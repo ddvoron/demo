21aa4947b75180d9f3e3905105c7926dfb440527
ddvoron <dd.versus@gmail.com>
2020-01-14 00:10:26 +0300

    1

--- src/main/java/com/example/demo/AutomaticController.java
+++ src/main/java/com/example/demo/AutomaticController.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.dto.RequestRegistry;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,7 +15,7 @@ public class AutomaticController {
 
     @PostMapping("reg")
     public ResponseEntity registry(@RequestBody RequestRegistry requestRegistry) {
-        //generate and save application number for specific application
+        //generate and save application number for specific application);
         System.out.println("called generation for app with applicationId = " + requestRegistry.getApplicationId());
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -26,6 +25,13 @@ public class AutomaticController {
         //get dbo value from application
         boolean dbo = true;
         System.out.println("called");
+       /* ObjectMapper mapper = new ObjectMapper();
+        try {
+            System.out.println(mapper.readValue("{\"applicationId\": 100}", RequestRegistry.class));
+            System.out.println(mapper.writeValueAsString(new RequestRegistry()));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }*/
         return new ResponseEntity<>(dbo,HttpStatus.OK);
     }
 
